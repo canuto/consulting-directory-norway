43789284ce0e09f547ff7b16f049381ed7cb783a
restdbjones <jones@restdb.io>
2025-03-14 15:12:18 +0100

    hbs layout

--- server.js
+++ server.js
@@ -2,16 +2,33 @@
 * Codehooks (c) example
 * A simple web site
 */
-import {app, Datastore} from 'codehooks-js'
+import {app, Datastore, aggregation} from 'codehooks-js'
 import handlebars from 'handlebars';
 import layouts from 'handlebars-layouts';
+import about from './web/templates/about.hbs';
+import contact from './web/templates/contact.hbs';
+import account from './web/templates/account.hbs';
+import index from './web/templates/index.hbs';
+import all from './web/templates/all.hbs';
+import category from './web/templates/category.hbs';
+import listing from './web/templates/listing.hbs';  
+import layout from './web/templates/layout.hbs';
+
+const templates = {
+    about: handlebars.compile(about),
+    contact: handlebars.compile(contact),
+    account: handlebars.compile(account),
+    index: handlebars.compile(index),
+    all: handlebars.compile(all),
+    category: handlebars.compile(category),
+    listing: handlebars.compile(listing),
+    layout: handlebars.compile(layout)
+}
 
 // Setting up the page views and layout template
 handlebars.registerHelper(layouts(handlebars));
+handlebars.registerPartial('layout', layout);
 
-app.set('views', '/web/templates');
-app.set('layout', '/web/templates/layout.hbs');
-app.set('view engine', {"hbs": handlebars});
 
 app.auth(/^\/(index|category|account|contact|about\/.*)?$/, (req, res, next) => { next() });
 
@@ -48,7 +65,7 @@ const loadCategoryFeatures = async (categorySlug) => {
 const loadListingById = async (id) => {
     const db = await Datastore.open();
     const listing = await db.getOne('listings', {_id: id});
-    console.log('listing', listing);
+    //console.log('listing', listing);
     return listing;
 };
 
@@ -65,70 +82,87 @@ const loadDirectories = async () => {
         }
     );
     const directories = {};
+    let totalCount = 0;  // Add counter variable
     await cursor.forEach((item) => {
-        if (!directories[item.directory]) {
-            directories[item.directory] = {name: item.directory, count: 0, categorySlug: item.categorySlug};
+        if (!directories[item.categorySlug]) {
+            directories[item.categorySlug] = {name: item.category, count: 0, categorySlug: item.categorySlug};
         }
-        directories[item.directory].name = item.directory;
-        directories[item.directory].count++;
+        directories[item.categorySlug].name = item.category;
+        directories[item.categorySlug].count++;
+        totalCount++;  // Increment total count
     })
+    // insert a new item for all categories with the total count
+    directories['all'] = {name: 'All categories', count: totalCount, categorySlug: 'all'};
     return Object.values(directories).sort((a, b) => b.count - a.count);
+};
+
+const loadAllCategories = async () => {
+    const db = await Datastore.open();    
+    const result = await db.getMany('listings', {}).toArray();
+    // group by category, storing the full items
+    const categories = result.reduce((acc, item) => {
+        if (!acc[item.category]) {
+            acc[item.category] = [];
+        }
+        acc[item.category].push(item);
+        return acc;
+    }, {});
+    return categories;
 };
 
 // Defining routes and page template to render for different pages
 app.get('/', async (req, res) => {
     const directories = await loadDirectories();
     const topFeatures = await loadTopFeatures();
-    console.log('directories', directories, 'topFeatures', topFeatures);
-    res.render('index', {directories, topFeatures, rand: rand()});
+    res.send(templates.index({directories, topFeatures, rand: rand()}));
+});
+
+app.get('/category/all', async (req, res) => {
+    const directories = await loadDirectories();
+    const allCategories = await loadAllCategories();
+    res.send(templates.all({directories, allCategories, rand: rand()}));
 });
 
 app.get('/category/:slug', async (req, res) => {
     const {slug} = req.params;
     const directories = await loadDirectories();
-    const categoryFeatures = await loadCategoryFeatures(slug);
-    
-    res.render('category', {
-        directories,
-        categoryFeatures,
-        category: slug,
-        rand: rand()
-    });
+    const categoryFeatures = await loadCategoryFeatures(slug);    
+    res.send(templates.category({directories, categoryFeatures, category: slug, rand: rand()}));
 });
 
 app.get('/listing/:slug/:id', async (req, res) => {
     const {slug, id} = req.params;
     const directories = await loadDirectories();
     const listing = await loadListingById(id);
-    res.render('listing', {listing, directories, rand: rand()});
+    res.send(templates.listing({listing, directories, rand: rand()}));
 });
 
 app.get('/account', async (req, res) => {
     console.log('account');
     const directories = await loadDirectories();
-    res.render('account', {directories, rand: rand()});
+    res.send(templates.account({directories, rand: rand()}));
 });
 
 app.get('/contact', async (req, res) => {
     console.log('contact');
     const directories = await loadDirectories();
-    res.render('contact', {directories, rand: rand()});
+    res.send(templates.contact({directories, rand: rand()}));
 });
 
 app.get('/about', async (req, res) => {
     console.log('about');
     const directories = await loadDirectories();
-    res.render('about', {directories, rand: rand()});
+    res.send(templates.about({directories, rand: rand()}));
 });
 
-app.static({route: "/", directory: "/web", notFound: "/404.html"} /*,(req, res, next) => {
+app.static({route: "/", directory: "/web", notFound: "/404.html"}, (_, res, next) => {
     console.log('If you see this, the client cache is invalidated or called for the first time');
     const ONE_HOUR =  1000*60*60;
     res.set('Cache-Control', `public, max-age=${ONE_HOUR}, s-maxage=${ONE_HOUR}`);
     res.setHeader("Expires", new Date(Date.now() + ONE_HOUR).toUTCString());
     res.removeHeader('Pragma');
     next();
-  }*/)
+  })
 
 // bind to serverless runtime
 export default app.init();
