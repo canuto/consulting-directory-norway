c465a01b4fc382fc8ea415575eadfdd394092453
Knut Martin Torn knutmt@gmail.com <knutmt@gmail.com>
2025-04-26 11:34:49 +0200

    released version

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-	"title": "Bergen Digital",
+	"title": "DigiHub Bergen",
 	"ingress": `		
-		Bergen Digital er en katalog med leverandører av digitale tjenester i Bergen. Vi har samlet små og store selskaper i Bergen som kan bistå med implementering av digitale løsninger (systemutvikling, apper, nettsider) og rådgivning. 
+		DigiHub Bergen er en katalog med leverandører av digitale tjenester i Bergen. Vi har samlet små og store selskaper i Bergen som kan bistå med implementering av digitale løsninger (systemutvikling, apper, nettsider) og rådgivning. 
 	`
 }
 export default settings;
